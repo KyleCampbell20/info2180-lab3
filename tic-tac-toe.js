46c02d65a075f067812fd117bf6b7b17f0e13b48
KyleCampbell20 <elykcampbell1@gmail.com>
2020-10-11 18:30:48 -0500

    Edited tic-tac-toe.js

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -9,24 +9,44 @@ window.onload = function addCname(){
     for (let i = 0 ; i < squares.length; i++){
         squares[i].classList.add("square");
         squares[i].addEventListener("click",function() {
-            
             console.log("square" + i + "clicked")
+            squares[i].addEventListener("mouseover", mousecheck)
+            squares[i].addEventListener("mouseout", MouseOcheck)
+            function mousecheck(){
+                squares[i].classList.add("hover")
+                squares[i].classList.remove("hover.O")
+                console.log("square" + i + "hovered")
+            }
+            function MouseOcheck(){
+                squares[i].classList.add("hover.O")
+                squares[i].classList.remove("hover")
+                console.log("square" + i + "Off")
+            }
+
             var container = document.getElementById("board")
-            var stateofGame = document.getElementsByClassName("square")
+            
+            
+            
+               
+                                       
             if(container.getElementsByClassName("square")[i].innerHTML != "X" && container.getElementsByClassName("square")[i].innerHTML != "O"){
                 container.getElementsByClassName("square")[i].innerHTML = currentP
                  if(currentP == "X"){
                     currentP = "O"
+                     
                     squares[i].classList.add("X")
+
+                    
                 }else if(currentP == "O"){
                     squares[i].classList.add("O")
                     currentP = "X"
+                    
                 } 
             }
             
                 
             
-        });
+        })
         
         
     }
