2fd8c16d8ad590618e9bd7c084a665d8e2b6d37a
KyleCampbell20 <elykcampbell1@gmail.com>
2020-10-12 00:42:28 -0500

    Edited tic-tac-toe.js

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -2,14 +2,16 @@
 window.onload = function addCname(){
     let squares = document.querySelectorAll("#board div");
     let currentP = "X";
-   
+    let stateofGame = ["", "", "", "", "", "", "", "", ""];
+    
     
     
     
     for (let i = 0 ; i < squares.length; i++){
-        squares[i].classList.add("square");
+        squares[i].classList.add("square")
         squares[i].addEventListener("click",function() {
             console.log("square" + i + "clicked")
+            
             squares[i].addEventListener("mouseover", mousecheck)
             squares[i].addEventListener("mouseout", MouseOcheck)
             function mousecheck(){
@@ -24,6 +26,8 @@ window.onload = function addCname(){
             }
 
             var container = document.getElementById("board")
+            var DisplayStatus = document.querySelector("#status")
+        
             
             
             
@@ -31,7 +35,48 @@ window.onload = function addCname(){
                                        
             if(container.getElementsByClassName("square")[i].innerHTML != "X" && container.getElementsByClassName("square")[i].innerHTML != "O"){
                 container.getElementsByClassName("square")[i].innerHTML = currentP
-                 if(currentP == "X"){
+                stateofGame[i] = currentP
+                console.log(stateofGame[i])
+                if(stateofGame[0] == currentP && stateofGame[1] == currentP && stateofGame[2] == currentP)
+                {
+                    document.getElementById("status").innerHTML = "Congratulations! " + currentP + " is the Winner! "
+                    document.getElementById("status").classList.add("you-won")
+                    
+        
+                }
+                if(stateofGame[0] == currentP && stateofGame[1] == currentP && stateofGame[2] == currentP){
+                    document.getElementById("status").innerHTML = "Congratulations! " + currentP + " is the Winner! "
+                    document.getElementById("status").classList.add("you-won")
+                } 
+                if(stateofGame[3] == currentP && stateofGame[4] == currentP && stateofGame[5] == currentP){
+                    document.getElementById("status").innerHTML = "Congratulations! " + currentP + " is the Winner! "
+                    document.getElementById("status").classList.add("you-won")
+                }
+                if(stateofGame[6] == currentP && stateofGame[7] == currentP && stateofGame[8] == currentP){
+                    document.getElementById("status").innerHTML = "Congratulations! " + currentP + " is the Winner! "
+                    document.getElementById("status").classList.add("you-won")
+                }
+                if(stateofGame[0] == currentP && stateofGame[3] == currentP && stateofGame[6] == currentP){
+                    document.getElementById("status").innerHTML = "Congratulations! " + currentP + " is the Winner! "
+                    document.getElementById("status").classList.add("you-won")
+                }
+                if(stateofGame[1] == currentP && stateofGame[4] == currentP && stateofGame[7] == currentP){
+                    document.getElementById("status").innerHTML = "Congratulations! " + currentP + " is the Winner! "
+                    document.getElementById("status").classList.add("you-won")
+                }
+                if(stateofGame[2] == currentP && stateofGame[5] == currentP && stateofGame[8] == currentP){
+                    document.getElementById("status").innerHTML = "Congratulations! " + currentP + " is the Winner! "
+                    document.getElementById("status").classList.add("you-won")
+                }
+                if(stateofGame[0] == currentP && stateofGame[4] == currentP && stateofGame[8] == currentP){
+                    document.getElementById("status").innerHTML = "Congratulations! " + currentP + " is the Winner! "
+                    document.getElementById("status").classList.add("you-won")
+                }
+                if(stateofGame[2] == currentP && stateofGame[4] == currentP && stateofGame[6] == currentP){
+                    document.getElementById("status").innerHTML = "Congratulations! " + currentP + " is the Winner! "
+                    document.getElementById("status").classList.add("you-won")
+                }
+                if(currentP == "X"){
                     currentP = "O"
                      
                     squares[i].classList.add("X")
@@ -49,8 +94,13 @@ window.onload = function addCname(){
         })
         
         
+        
+
+        
+        
     }
-    
+ 
+   
 }    
     
 
