d8b67aa664d9a6a8964099b9a58836d8b1ae822f
KyleCampbell20 <elykcampbell1@gmail.com>
2020-10-12 13:45:11 -0500

    Edited tic-tac-toe.js(Add X or O)

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,6 +1,10 @@
 "use strict";
 window.onload = function addCname(){
     let squares = document.querySelectorAll("#board div");
+    let currentP = "X";
+    let stateofGame = ["", "", "", "", "", "", "", "", ""];
+    let btn = document.querySelector(".btn")
+    var container = document.getElementById("board")
     
     
     for (let i = 0 ; i < squares.length; i++){
@@ -8,11 +12,39 @@ window.onload = function addCname(){
         squares[i].addEventListener("click",function() {
             console.log("square" + i + "clicked")
             
+            if(container.getElementsByClassName("square")[i].innerHTML != "X" && container.getElementsByClassName("square")[i].innerHTML != "O"){
+                container.getElementsByClassName("square")[i].innerHTML = currentP
+                stateofGame[i] = currentP
+                console.log(stateofGame[i])    
             
-            
+                if(currentP == "X"){
+                    currentP = "O"
+                    squares[i].classList.add("X")
+        
+                }else if(currentP == "O"){
+                    squares[i].classList.add("O")
+                    currentP = "X"
+                } 
+            }
             
            
             
+
+            
+            
+        
+            
+            
+            
+               
+                                       
+            
+                
+               
+                
+        
+                
+    
                 
             
             
